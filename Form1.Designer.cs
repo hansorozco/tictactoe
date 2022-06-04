b409bf2f2ed899998d5ad1f2eb5478423b4c399c
Hans Orozco <104253434+hansorozco@users.noreply.github.com>
2022-06-05 01:37:37 +0800

    added logo

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -43,6 +43,7 @@
             this.C1 = new System.Windows.Forms.Button();
             this.C2 = new System.Windows.Forms.Button();
             this.C3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.menuStrip2.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -109,7 +110,7 @@
             this.A1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.A1.Font = new System.Drawing.Font("ROG Fonts", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.A1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.A1.Location = new System.Drawing.Point(129, 81);
+            this.A1.Location = new System.Drawing.Point(141, 91);
             this.A1.Name = "A1";
             this.A1.Size = new System.Drawing.Size(166, 149);
             this.A1.TabIndex = 2;
@@ -122,7 +123,7 @@
             this.A2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.A2.Font = new System.Drawing.Font("ROG Fonts", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.A2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.A2.Location = new System.Drawing.Point(358, 81);
+            this.A2.Location = new System.Drawing.Point(370, 91);
             this.A2.Name = "A2";
             this.A2.Size = new System.Drawing.Size(166, 149);
             this.A2.TabIndex = 3;
@@ -135,7 +136,7 @@
             this.A3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.A3.Font = new System.Drawing.Font("ROG Fonts", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.A3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.A3.Location = new System.Drawing.Point(570, 81);
+            this.A3.Location = new System.Drawing.Point(582, 91);
             this.A3.Name = "A3";
             this.A3.Size = new System.Drawing.Size(166, 149);
             this.A3.TabIndex = 4;
@@ -148,7 +149,7 @@
             this.B1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.B1.Font = new System.Drawing.Font("ROG Fonts", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.B1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.B1.Location = new System.Drawing.Point(129, 255);
+            this.B1.Location = new System.Drawing.Point(141, 265);
             this.B1.Name = "B1";
             this.B1.Size = new System.Drawing.Size(166, 149);
             this.B1.TabIndex = 5;
@@ -161,7 +162,7 @@
             this.B2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.B2.Font = new System.Drawing.Font("ROG Fonts", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.B2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.B2.Location = new System.Drawing.Point(358, 255);
+            this.B2.Location = new System.Drawing.Point(370, 265);
             this.B2.Name = "B2";
             this.B2.Size = new System.Drawing.Size(166, 149);
             this.B2.TabIndex = 6;
@@ -174,7 +175,7 @@
             this.B3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.B3.Font = new System.Drawing.Font("ROG Fonts", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.B3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.B3.Location = new System.Drawing.Point(570, 255);
+            this.B3.Location = new System.Drawing.Point(582, 265);
             this.B3.Name = "B3";
             this.B3.Size = new System.Drawing.Size(166, 149);
             this.B3.TabIndex = 7;
@@ -187,7 +188,7 @@
             this.C1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.C1.Font = new System.Drawing.Font("ROG Fonts", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.C1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.C1.Location = new System.Drawing.Point(129, 439);
+            this.C1.Location = new System.Drawing.Point(141, 449);
             this.C1.Name = "C1";
             this.C1.Size = new System.Drawing.Size(166, 149);
             this.C1.TabIndex = 8;
@@ -200,7 +201,7 @@
             this.C2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.C2.Font = new System.Drawing.Font("ROG Fonts", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.C2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.C2.Location = new System.Drawing.Point(358, 439);
+            this.C2.Location = new System.Drawing.Point(370, 449);
             this.C2.Name = "C2";
             this.C2.Size = new System.Drawing.Size(166, 149);
             this.C2.TabIndex = 9;
@@ -213,12 +214,24 @@
             this.C3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.C3.Font = new System.Drawing.Font("ROG Fonts", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.C3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.C3.Location = new System.Drawing.Point(570, 439);
+            this.C3.Location = new System.Drawing.Point(582, 449);
             this.C3.Name = "C3";
             this.C3.Size = new System.Drawing.Size(166, 149);
             this.C3.TabIndex = 10;
             this.C3.UseVisualStyleBackColor = false;
             this.C3.Click += new System.EventHandler(this.button_click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("ROG Fonts", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Crimson;
+            this.label1.Location = new System.Drawing.Point(226, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(469, 44);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "STRIX TIC TAC TOE";
             // 
             // Form1
             // 
@@ -226,7 +239,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(926, 612);
+            this.ClientSize = new System.Drawing.Size(926, 617);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.C3);
             this.Controls.Add(this.C2);
             this.Controls.Add(this.C1);
@@ -263,6 +277,7 @@
         private System.Windows.Forms.Button C2;
         private System.Windows.Forms.Button C3;
         private System.Windows.Forms.ToolStripMenuItem aBOUTToolStripMenuItem1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
